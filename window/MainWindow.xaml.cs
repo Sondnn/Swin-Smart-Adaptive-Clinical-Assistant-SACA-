80644c49d821a3d3d74dc19b166c11e47d2fa2c8
Văn Công Thành <vancongthanh109@gmail.com>
2026-04-28 12:26:04 +1000

    feat: added the initial design for Triage Window app and mock test API

---
+++ window/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SACA.WindowsApp.Models;
+using SACA.WindowsApp.Services;
+using SACA.WindowsApp.Pages;
+
+namespace SACA.WindowsApp
+{
+    public partial class MainWindow : Window
+    {
+        public TriageRequest CurrentRequest { get; set; } = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NavigateToSplash();
+        }
+
+        public void NavigateToSplash()
+        {
+            MainContent.Content = new SplashPage(this);
+        }
+
+        public void NavigateToStart()
+        {
+            MainContent.Content = new StartPage(this);
+        }
+
+        public void NavigateToPatientInfo()
+        {
+            MainContent.Content = new PatientInfoPage(this);
+        }
+
+        public void NavigateToSymptoms()
+        {
+            MainContent.Content = new SymptomsPage(this);
+        }
+
+        public void NavigateToDuration()
+        {
+            MainContent.Content = new DurationPage(this);
+        }
+
+        public void NavigateToLoading()
+        {
+            MainContent.Content = new LoadingPage(this);
+        }
+
+        public void NavigateToResult(TriageResponse response)
+        {
+            MainContent.Content = new ResultPage(this, response);
+        }
+    }
+}
---
+++ window/Models/TriageResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SACA.WindowsApp.Models
+{
+    public class TriageResponse
+    {
+        public string TriageLevel { get; set; } = "";
+        public string Recommendation { get; set; } = "";
+        public double Confidence { get; set; }
+        public List<string> Summary { get; set; } = new();
+    }
+}
