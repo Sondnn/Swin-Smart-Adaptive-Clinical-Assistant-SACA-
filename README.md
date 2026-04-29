447659b774ce7aeed66f572a18d85f03f4cd8a4f
Son Dinh Nguyen Nhat <dinhnguyennhatson@icloud.com>
2026-04-29 19:11:45 +1000

    Fix: README.md and router.py

--- README.md
+++ README.md
@@ -39,9 +39,11 @@ This will:
 #### API Service
 
 ```bash
+cd backend
 python3 -m uvicorn main:app --reload
 ```
 The API will be available at `http://127.0.0.1:8000`
+Testing in `http://127.0.0.1:8000/docs`
 
 ## Repository Layout
 
--- backend/api/router.py
+++ backend/api/router.py
@@ -8,16 +8,6 @@ from ml.ml_service import MLService
 router = APIRouter()
 ml_service = MLService(MODEL_DIR)
 
-class Predictrequest(BaseModel):
-    gender: int
-    pregnant: int
-    is_child_under_12: int
-    age_over_65: int
-    duration_hours: int
-    pain_score: int
-    previous_major_condition: Optional[int] = 0
-    symptoms: Optional[List[str]] = []
-
 @router.get("/")
 def root():
     return {"message": "SACA Backend API is running"}
